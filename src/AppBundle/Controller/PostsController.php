aaa8fb0f491b7a475e82ee8429933d7b9dd0e1e2
dantleech <dan.t.leech@gmail.com>
2016-08-05 07:17:40 +0100

    Frontend

--- src/AppBundle/Controller/PostsController.php
+++ src/AppBundle/Controller/PostsController.php
@@ -2,12 +2,35 @@
 
 namespace AppBundle\Controller;
 
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\HttpFoundation\Response;
+use Doctrine\ORM\EntityManagerInterface;
+use AppBundle\Entity\Post;
+
 class PostsController
 {
+    private $templating;
+    private $manager;
+
+    public function __construct(
+        EngineInterface $templating,
+        EntityManagerInterface $manager
+    )
+    {
+        $this->templating = $templating;
+        $this->manager = $manager;
+    }
+
     public function indexAction(Request $request)
     {
-        return $this->twig->render(
-            '@AppBundle/posts/index.html.twig'
-        );
+        $posts = $this->manager->getRepository(Post::class)->findAll();
+
+        return new Response($this->templating->render(
+            '@App/posts/index.html.twig',
+            [
+                'posts' => $posts
+            ]
+        ));
     }
 }
